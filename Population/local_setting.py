520870effbe8fd910bb191674c3a7bb1f2038da0
Mara Green <maragreen42@gmail.com>
2023-07-15 11:32:09 -0700

    create Population_database in mysql + create Population_app

---
+++ Population/local_setting.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Population_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Payoffdebt1!',
+    }
+}
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-c%lu*vsnf1z-f=sq^@@kpv*pu8r8-9+!q^ns7s=a@@3ja6&(c*'
--- Population/settings.py
+++ Population/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c%lu*vsnf1z-f=sq^@@kpv*pu8r8-9+!q^ns7s=a@@3ja6&(c*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +71,6 @@ WSGI_APPLICATION = 'Population.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -121,3 +113,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from Population.local_settings import *
+except ImportError:
+    pass
