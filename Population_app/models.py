cf2db4c2d60f88537ce48910e57803216d3db996
Mara Green <maragreen42@gmail.com>
2023-07-15 11:52:37 -0700

    added models.py and serilaizers.py

--- Population_app/models.py
+++ Population_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Population(models.Model):
+    name=models.CharField(max_length=255)
+    country=models.CharField(max_length=255)
+    population=models.IntegerField()
+    limit=models.IntegerField()
